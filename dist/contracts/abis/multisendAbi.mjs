6038353135838d477383f7dfd9201b88df28a0a8
YogaaJS <dev.yogaa@gmail.com>
2025-12-11 20:11:21 +0000

    Update version to 0.0.3 and refactor various contract ABIs and encoder files for consistency

--- dist/contracts/abis/multisendAbi.mjs
+++ dist/contracts/abis/multisendAbi.mjs
@@ -1,1 +1,1 @@
-const t=[{constant:!1,inputs:[{internalType:"bytes",name:"transactions",type:"bytes"}],name:"multiSend",outputs:[],payable:!1,stateMutability:"nonpayable",type:"function"}];export{t as multisendAbi};
+var t=[{constant:!1,inputs:[{internalType:"bytes",name:"transactions",type:"bytes"}],name:"multiSend",outputs:[],payable:!1,stateMutability:"nonpayable",type:"function"}];export{t as multisendAbi};
--- dist/contracts/constants.mjs
+++ dist/contracts/constants.mjs
@@ -1,1 +1,1 @@
-const o=6,t="Polymarket Contract Proxy Factory";export{t as SAFE_FACTORY_NAME,o as USDCE_DIGITS};
+var o=6,t="Polymarket Contract Proxy Factory";export{t as SAFE_FACTORY_NAME,o as USDCE_DIGITS};
